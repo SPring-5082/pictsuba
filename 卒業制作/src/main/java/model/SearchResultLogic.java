93d0f01025b7f055222d65ec9e899c87ae71696a
syaro910 <ryuseifgo@gmail.com>
2024-12-11 10:01:25 +0900

    SearchResultLogicの変更

---
+++ 卒業制作/src/main/java/model/SearchResultLogic.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.List;
+
+import beans.Product;
+
+public class SearchResultLogic {
+	public static List<Product> execute(List<Product> productList, String searchWord) {
+		
+		
+		
+		for(Product product : productList) {
+			
+		}
+		
+		
+		return productList;
+	}
+}
--- 卒業制作/src/main/java/servlet/SearchServlet.java
+++ 卒業制作/src/main/java/servlet/SearchServlet.java
@@ -1,14 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import beans.Product;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import model.SearchResult;
+import model.SearchResultLogic;
 @WebServlet("/search")
 public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -19,7 +21,7 @@ public class SearchServlet extends HttpServlet {
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("list", SearchResult.execute(request, response));
+		request.setAttribute("list", SearchResultLogic.execute(new ArrayList<Product>(), "検索ワード"));
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/search.jsp");
 		dispatcher.forward(request, response);
